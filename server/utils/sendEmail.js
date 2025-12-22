f2578115a552961bf79f602f3f435c431335ad80
Mishal010 <153232369+Mishal010@users.noreply.github.com>
2025-12-22 19:18:38 +0530

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,9 +2,9 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: String(process.env.EMAIL_SECURE) === "true",
+  service: process.env.EMAIL_HOST,
+//  port: Number(process.env.EMAIL_PORT),
+//  secure: String(process.env.EMAIL_SECURE) === "true",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
